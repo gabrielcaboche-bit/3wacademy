be2c36d5bed95a15d567f15888d48e1f04b84a32
gabrielcaboche-bit <gabriel.caboche@3wa.io>
2025-11-17 10:33:43 +0100

    Create index.php

---
+++ B1/portfolio/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <tilte>Portfolio | Gabriel CABOCHE</tilte>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <main>
+        <div>
+            <h1>BIENVENUE</h1>
+        
+        <button>mon CV</button>
+    </main>
+    <footer>
+    </footer>
+</body>
+
+</html>
