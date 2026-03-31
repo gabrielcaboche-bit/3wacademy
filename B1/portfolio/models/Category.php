deefca0cd4f92395ddf657abdade279b58bfd7c0
gabriel C <gabcab1002@gmail.com>
2026-03-31 18:19:51 +0200

    LINUX

--- B1/portfolio/models/Category.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Models;
-
-class Category {
-    public ?int $id = null;
-    public string $name = '';
-}
--- B1/portfolio/models/Skill.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Models;
-
-class Skill {
-    public ?int $id = null;
-    public string $name = '';
-    public int $level = 0;
-}
